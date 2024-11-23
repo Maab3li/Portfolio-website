5e12c891fecafe0d3467a581c5775ddd9296d6d2
Maab3li <maab3li555@gmail.com>
2024-11-22 17:57:11 -0800

    updated the code

---
+++ Portfolio website/js/plugins.js
@@ -0,0 +1,34 @@
+$('.container').css("width","1200")
+$('.container').css("letter-spacing","3px")
+$('.row1').css("height","300px")
+$('.row2').css("height","370px")
+$('.row0').css("height","600px")
+$('.row3').css("height","520px")
+$('.row4').css("height","350px")
+$('.row5').css("height","230px")
+$('.row6').css("height","150px")
+$('.row7').css("height","100px")
+$('h3',).css("color","#FFF")
+$('h2',).css("color","#FFF")
+$('footer',).css("color","#FFF")
+$("a").css("color","#FFF")
+$("button").hide()
+$(".b0").show()
+$(".b0").click(function(){
+$(this).hide(300)
+$("h3").show(2000)
+$("p").show(2000)
+$("ul").show(2000)
+$("a").show(2000)
+$(".image").show(800)
+})
+$(".b0").hover(
+    (function(){
+        $(".b0").addClass("button-hover")
+    }),
+    (function(){
+        $(".b0").removeClass("button-hover")
+    })
+)
+
+
