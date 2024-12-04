67a4237656becae25ebf33fe96a4cd662cb8c4a3
Maab3li <maab3li555@gmail.com>
2024-12-04 13:02:28 -0800

    updated the code

--- Portfolio website/js/plugins.js
+++ Portfolio website/js/plugins.js
@@ -1,10 +1,10 @@
 $('.container').css("width","1200")
 $('.container').css("letter-spacing","3px")
-$('.row1').css("height","300px")
-$('.row2').css("height","370px")
+$('.row1').css("height","450px")
+$('.row2').css("height","500px")
 $('.row0').css("height","600px")
-$('.row3').css("height","520px")
-$('.row4').css("height","350px")
+$('.row3').css("height","1000px")
+$('.row4').css("height","470px")
 $('.row5').css("height","230px")
 $('.row6').css("height","150px")
 $('.row7').css("height","100px")
@@ -12,23 +12,64 @@ $('h3',).css("color","#FFF")
 $('h2',).css("color","#FFF")
 $('footer',).css("color","#FFF")
 $("a").css("color","#FFF")
-$("button").hide()
-$(".b0").show()
-$(".b0").click(function(){
-$(this).hide(300)
-$("h3").show(2000)
-$("p").show(2000)
-$("ul").show(2000)
-$("a").show(2000)
-$(".image").show(800)
-})
-$(".b0").hover(
-    (function(){
-        $(".b0").addClass("button-hover")
-    }),
-    (function(){
-        $(".b0").removeClass("button-hover")
-    })
-)
+
+
+
+
+
+
+function showFirst(){
+    $('h1').show(600, function(){
+        $('.img1').show(700, function(){
+            $('.h2').show(800, function(){
+                $('.img2').show(1000, function(){
+                    $('.header1').show(1200, function(){
+                        $('.p1').show(2000, function(){
+                            $('.img3').show(1300, function(){
+                                $('.header2').show(2300, function(){
+                                    $('.img4').show(1000, function(){
+                                        $('.header3').show(1500, function(){
+                                            $('.p2').show(2000, function(){
+                                                $('.img5').show(1000, function(){
+                                                    $('.header4').show(1000, function(){
+                                                        $('.pu').show(500, function(){
+                                                        $('.p3').show(1100, function(){
+                                                            $('.p4').show(1100, function(){
+                                                                $('.p5').show(1100, function(){
+                                                                    $('.p6').show(1700, function(){
+                                                                    $('.header5').show(1700, function(){
+                                                                        $('.p7').show(1000, function(){
+                                                                            $('.p8').show(1000, function(){
+                                                                            $('.header6').show(1900, function(){
+                                                                                $('.p9').show(1100, function(){
+                                                                                    $('.p10').show(1100, function(){
+                                                                                        $('.p11').show(1100)
+                                                                                    })
+                                                                                })
+                                                                            })
+                                                                        })
+                                                                    })
+                                                                })
+                                                            })
+                                                        })
+                                                    })
+                                                })
+                                            })
+                                        })
+                                    })
+                                })
+                            })
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    }
+
+        showFirst()
 
 
