8c9b844397efe859f995ef7a4eb1d80355ac5f2c
Maab3li <maab3li555@gmail.com>
2024-12-05 09:03:35 -0800

    updated the code

---
+++ Portfolio website/js/main.js
@@ -0,0 +1,274 @@
+
+// who iam
+let text2 = new SplitType('#text2')
+let characters2 = document.querySelectorAll('.char')
+
+for(i=0; i<characters2.length; i++) {
+    characters2[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :3,
+    duration:0.5
+})
+
+//about me
+let text3 = new SplitType('#text3')
+let characters3 = document.querySelectorAll('.char')
+
+for(i=0; i<characters3.length; i++) {
+    characters3[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :3,
+    duration:0.5
+})
+
+// about me paragraph
+let text4 = new SplitType('#text4')
+let characters4 = document.querySelectorAll('.char')
+
+for(i=0; i<characters4.length; i++) {
+    characters4[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.5,
+    delay :3,
+    duration:0.5
+})
+
+// my hobbies
+let text5 = new SplitType('#text5')
+let characters5 = document.querySelectorAll('.char')
+
+for(i=0; i<characters5.length; i++) {
+    characters5[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :3,
+    duration:0.5
+})
+
+// my education
+let text6 = new SplitType('#text6')
+let characters6 = document.querySelectorAll('.char')
+
+for(i=0; i<characters6.length; i++) {
+    characters6[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :3,
+    duration:0.5
+})
+
+// my education paragraph
+let text7 = new SplitType('#text7')
+let characters7 = document.querySelectorAll('.char')
+
+for(i=0; i<characters7.length; i++) {
+    characters7[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :3,
+    duration:1
+})
+
+//my qualifications
+let text8 = new SplitType('#text8')
+let characters8 = document.querySelectorAll('.char')
+
+for(i=0; i<characters8.length; i++) {
+    characters8[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.10,
+    delay :3,
+    duration:0.5
+})
+
+/* my qualification list start*/
+let text9 = new SplitType('#text9')
+let characters9 = document.querySelectorAll('.char')
+
+for(i=0; i<characters9.length; i++) {
+    characters9[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :3,
+    duration:0.5
+})
+
+let text10 = new SplitType('#text10')
+let characters10 = document.querySelectorAll('.char')
+
+for(i=0; i<characters10.length; i++) {
+    characters10[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :3,
+    duration:0.5
+})
+
+let text11 = new SplitType('#text11')
+let characters11 = document.querySelectorAll('.char')
+
+for(i=0; i<characters11.length; i++) {
+    characters11[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :3,
+    duration:0.5
+})
+
+
+let text12 = new SplitType('#text12')
+let characters12 = document.querySelectorAll('.char')
+
+for(i=0; i<characters12.length; i++) {
+    characters12[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :3,
+    duration:0.5
+})
+/*my qualification list end*/
+
+
+// my codings
+let text13 = new SplitType('#text13')
+let characters13 = document.querySelectorAll('.char')
+
+for(i=0; i<characters13.length; i++) {
+    characters13[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :3,
+    duration:0.5
+})
+
+//available on github 
+let text14 = new SplitType('#text14')
+let characters14 = document.querySelectorAll('.char')
+
+for(i=0; i<characters14.length; i++) {
+    characters14[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :3,
+    duration:0.5
+})
+
+//github link 
+let text15 = new SplitType('#text15')
+let characters15 = document.querySelectorAll('.char')
+
+for(i=0; i<characters15.length; i++) {
+    characters15[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :4,
+    duration:0.5
+})
+
+//contact
+let text16 = new SplitType('#text16')
+let characters16 = document.querySelectorAll('.char')
+
+for(i=0; i<characters16.length; i++) {
+    characters16[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :4,
+    duration:0.5
+})
+
+//email
+let text17 = new SplitType('#text17')
+let characters17 = document.querySelectorAll('.char')
+
+for(i=0; i<characters17.length; i++) {
+    characters17[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :4,
+    duration:0.5
+})
+
+//linkedin
+let text18 = new SplitType('#text18')
+let characters18 = document.querySelectorAll('.char')
+
+for(i=0; i<characters18.length; i++) {
+    characters18[i].classList.add('translate-y-full')
+}
+
+gsap.to('.char', {
+    y:0,
+    x:0,
+    stagger: 0.05,
+    delay :4,
+    duration:0.5
+})
+
+
--- Portfolio website/js/plugins.js
+++ Portfolio website/js/plugins.js
@@ -1,9 +1,9 @@
 $('.container').css("width","1200")
 $('.container').css("letter-spacing","3px")
 $('.row1').css("height","450px")
-$('.row2').css("height","500px")
+$('.row2').css("height","520px")
 $('.row0').css("height","600px")
-$('.row3').css("height","1000px")
+$('.row3').css("height","1100px")
 $('.row4').css("height","470px")
 $('.row5').css("height","230px")
 $('.row6').css("height","150px")
@@ -12,6 +12,9 @@ $('h3',).css("color","#FFF")
 $('h2',).css("color","#FFF")
 $('footer',).css("color","#FFF")
 $("a").css("color","#FFF")
+$('h2').css('font-size','30px')
+$('h2').css('margin-bottom','10px')
+$('p').css('margin-bottom','10px')
 
 
 
@@ -27,11 +30,11 @@ function showFirst(){
                         $('.p1').show(2000, function(){
                             $('.img3').show(1300, function(){
                                 $('.header2').show(2300, function(){
-                                    $('.img4').show(1000, function(){
+                                    $('.img4').show(1500, function(){
                                         $('.header3').show(1500, function(){
                                             $('.p2').show(2000, function(){
                                                 $('.img5').show(1000, function(){
-                                                    $('.header4').show(1000, function(){
+                                                    $('.header4').show(1300, function(){
                                                         $('.pu').show(500, function(){
                                                         $('.p3').show(1100, function(){
                                                             $('.p4').show(1100, function(){
@@ -41,8 +44,8 @@ function showFirst(){
                                                                         $('.p7').show(1000, function(){
                                                                             $('.p8').show(1000, function(){
                                                                             $('.header6').show(1900, function(){
-                                                                                $('.p9').show(1100, function(){
-                                                                                    $('.p10').show(1100, function(){
+                                                                                $('.p9').show(1000, function(){
+                                                                                    $('.p10').show(1000, function(){
                                                                                         $('.p11').show(1100)
                                                                                     })
                                                                                 })
